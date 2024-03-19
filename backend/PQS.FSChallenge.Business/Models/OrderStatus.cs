065fbc5f827dce375a5b97456311b8d1b3ec1951
nicolas.ramirez <nicolas.ramirez@NRAMIREZ-PC>
2024-03-19 13:19:02 -0300

    OrderStatus enum agregado

---
+++ backend/PQS.FSChallenge.Business/Models/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PQS.FSChallenge.Business.Models
+{
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Approved = 1,
+        Rejected = -1
+    }
+}
